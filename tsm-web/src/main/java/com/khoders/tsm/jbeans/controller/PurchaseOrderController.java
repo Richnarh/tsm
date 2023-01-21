94e8a0ad0bc63cc162310f7adf81df231c6f1327
Richard Narh <pascalcppjava@gmail.com>
2023-01-21 23:48:30 +0000

    Updates  on adding sales service

---
+++ tsm-web/src/main/java/com/khoders/tsm/jbeans/controller/PurchaseOrderController.java
@@ -0,0 +1,309 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.khoders.tsm.jbeans.controller;
+
+import com.khoders.tsm.services.InventoryService;
+import com.khoders.resource.jpa.CrudApi;
+import com.khoders.resource.utilities.CollectionList;
+import com.khoders.resource.utilities.FormView;
+import com.khoders.resource.utilities.Msg;
+import com.khoders.resource.utilities.SystemUtils;
+import com.khoders.tsm.entities.ProductPackage;
+import com.khoders.tsm.entities.PurchaseOrder;
+import com.khoders.tsm.entities.PurchaseOrderItem;
+import com.khoders.tsm.listener.AppSession;
+import com.khoders.tsm.services.SalesService;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author richard
+ */
+@Named(value = "purchaseOrderController")
+@SessionScoped
+public class PurchaseOrderController implements Serializable
+{
+    @Inject private CrudApi crudApi;
+    @Inject private InventoryService inventoryService;
+    @Inject private AppSession appSession;
+    @Inject private SalesService salesService;
+    
+    private PurchaseOrder purchaseOrder = new PurchaseOrder();
+    private List<PurchaseOrder> purchaseOrderList = new LinkedList<>();
+    
+    private PurchaseOrderItem purchaseOrderItem = new PurchaseOrderItem();
+    private List<PurchaseOrderItem> purchaseOrderItemList = new LinkedList<>();
+    private List<PurchaseOrderItem> removedOrderItemList = new LinkedList<>();
+    private List<ProductPackage> productPackageList = new LinkedList<>();
+    
+    private FormView pageView = FormView.listForm();
+    private String optionText;
+    
+     private double totalAmount = 0.0;
+    
+    @PostConstruct
+    private void init()
+    {
+        purchaseOrderList = inventoryService.getPurchaseOrderList();
+        
+        clearPurchaseOrder();
+    }
+    
+    public void initPurchaseOrder()
+    {
+        clearPurchaseOrder();
+        pageView.restToCreateView();
+    }
+    
+    public void packageData()
+    {
+        productPackageList = salesService.queryPackagePrice(purchaseOrderItem.getProduct());
+    }
+    
+    
+    public void savePurchaseOrder()
+    {
+        try
+        {
+           purchaseOrder.genOrderCode();
+           if(crudApi.save(purchaseOrder)!=null)
+           {
+               purchaseOrderList = CollectionList.washList(purchaseOrderList, purchaseOrder);
+               Msg.info("Purchase order saved");
+               clearPurchaseOrder();
+           }
+           
+           closePage();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+  public void deletePurchaseOrder(PurchaseOrder purchaseOrder)
+    {
+        try 
+        {
+          if(crudApi.delete(purchaseOrder))
+          {
+              purchaseOrderList.remove(purchaseOrder);
+              Msg.info(Msg.SUCCESS_MESSAGE);
+          }
+          else
+          {
+            Msg.info(Msg.FAILED_MESSAGE);
+          }
+        } catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+  
+    public void managePurchaseOrderItem(PurchaseOrder purchaseOrder)
+    {
+        this.purchaseOrder = purchaseOrder;
+        pageView.restToDetailView();
+        totalAmount = 0.0;
+        clearPurchaseOrderItem();
+        
+        purchaseOrderItemList = inventoryService.getPurchaseOrderItem(purchaseOrder);
+              
+        for (PurchaseOrderItem items : purchaseOrderItemList) 
+        {
+            totalAmount += (items.getQtyPurchased() * items.getCostPrice());
+        }
+    }
+
+    public void addPurchaseOrderItem()
+    {
+        try
+        {
+            Predicate<PurchaseOrderItem> predicate = orderItem -> orderItem.getQtyPurchased() <= 0;
+            
+            if(predicate.test(purchaseOrderItem))
+            {
+              Msg.error("Please enter quantity");
+              return;
+            }
+            
+            if (purchaseOrderItem.getCostPrice() <= 0.0) {
+                Msg.info("Please enter price");
+                return;
+            }
+
+            if (purchaseOrderItem != null) {
+               
+                totalAmount += purchaseOrderItem.getQtyPurchased() * purchaseOrderItem.getCostPrice();
+                
+                purchaseOrderItemList.add(purchaseOrderItem);
+                purchaseOrderItemList = CollectionList.washList(purchaseOrderItemList, purchaseOrderItem);
+
+                Msg.info("Order item added");
+            } else {
+                Msg.info("Order item removed!");
+            }
+            clearPurchaseOrderItem();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void saveAll()
+    {
+       try 
+        {
+            for (PurchaseOrderItem orderItem : purchaseOrderItemList)
+            {
+//                if (totalAmount != purchaseOrder.getTotalAmount())
+//                {
+//                    FacesContext.getCurrentInstance().addMessage(null,
+//                            new FacesMessage(FacesMessage.SEVERITY_ERROR, Msg.setMsg("The item total sum: " + (totalAmount) + " is not equivalent to the purchase order total: " + purchaseOrder.getTotalAmount()), null));
+//                        return;
+//                }
+                orderItem.genCode();
+                orderItem.setPurchaseOrder(purchaseOrder);
+                orderItem.setSubTotal(orderItem.getQtyPurchased() * orderItem.getCostPrice());
+                crudApi.save(orderItem);
+            }
+
+            for (PurchaseOrderItem orderItem : removedOrderItemList)
+            {
+                crudApi.delete(orderItem);
+                removedOrderItemList.remove(orderItem);
+            }
+            double total = purchaseOrderItemList.stream().mapToDouble(PurchaseOrderItem::getSubTotal).sum();
+            purchaseOrder.setTotalAmount(total);
+            crudApi.save(purchaseOrder);
+            System.out.println("Remove order size after -- " + removedOrderItemList.size());
+            
+            closePage();
+            
+            Msg.info("Purchase order item list saved!");
+        } catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public void editPurchaseOrderItem(PurchaseOrderItem purchaseOrderItem)
+    {
+        this.purchaseOrderItem = purchaseOrderItem;
+        totalAmount -= (purchaseOrderItem.getQtyPurchased() * purchaseOrderItem.getCostPrice());
+        purchaseOrderItemList.remove(purchaseOrderItem);
+    }
+    
+    public void removePurchaseOrderItem(PurchaseOrderItem purchaseOrderItem)
+    {
+        totalAmount -= (purchaseOrderItem.getQtyPurchased() * purchaseOrderItem.getCostPrice());
+        removedOrderItemList = CollectionList.washList(removedOrderItemList, purchaseOrderItem);
+        purchaseOrderItemList.remove(purchaseOrderItem);
+        
+        System.out.println("Size on removing --- "+removedOrderItemList.size());
+    }
+        
+    public void editPurchaseOrder(PurchaseOrder purchaseOrder) 
+    {
+       pageView.restToCreateView();
+       this.purchaseOrder=purchaseOrder;
+       optionText = "Update";
+    }
+    
+    public void clearPurchaseOrder() 
+    {
+        purchaseOrder = new PurchaseOrder();
+        purchaseOrder.setUserAccount(appSession.getCurrentUser());
+        purchaseOrder.genCode();
+        optionText = "Save Changes";
+        SystemUtils.resetJsfUI();
+    }
+    
+    public void closePage()
+    {
+       purchaseOrder = new PurchaseOrder();
+       optionText = "Save Changes";
+       pageView.restToListView();
+    }
+    public void clearPurchaseOrderItem()
+    {
+        purchaseOrderItem = new PurchaseOrderItem();
+        optionText = "Save Changes";
+        purchaseOrderItem.setPurchaseOrder(purchaseOrder);
+        SystemUtils.resetJsfUI();
+    }
+    
+    public PurchaseOrder getPurchaseOrder()
+    {
+        return purchaseOrder;
+    }
+
+    public void setPurchaseOrder(PurchaseOrder purchaseOrder)
+    {
+        this.purchaseOrder = purchaseOrder;
+    }
+
+    public PurchaseOrderItem getPurchaseOrderItem()
+    {
+        return purchaseOrderItem;
+    }
+
+    public void setPurchaseOrderItem(PurchaseOrderItem purchaseOrderItem)
+    {
+        this.purchaseOrderItem = purchaseOrderItem;
+    }
+
+    public List<PurchaseOrder> getPurchaseOrderList()
+    {
+        return purchaseOrderList;
+    }
+
+    public List<PurchaseOrderItem> getPurchaseOrderItemList()
+    {
+        return purchaseOrderItemList;
+    }
+
+    public FormView getPageView()
+    {
+        return pageView;
+    }
+
+    public void setPageView(FormView pageView)
+    {
+        this.pageView = pageView;
+    }
+
+    public String getOptionText()
+    {
+        return optionText;
+    }
+
+    public void setOptionText(String optionText)
+    {
+        this.optionText = optionText;
+    }
+
+    public double getTotalAmount()
+    {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount)
+    {
+        this.totalAmount = totalAmount;
+    }
+
+    public List<ProductPackage> getProductPackageList()
+    {
+        return productPackageList;
+    }
+    
+}
--- tsm-web/src/main/java/com/khoders/tsm/services/SalesService.java
+++ tsm-web/src/main/java/com/khoders/tsm/services/SalesService.java
@@ -13,6 +13,9 @@ import com.khoders.tsm.enums.CustomerType;
 import com.khoders.tsm.listener.AppSession;
 import com.khoders.resource.jpa.CrudApi;
 import com.khoders.resource.utilities.DateRangeUtil;
+import com.khoders.tsm.entities.Product;
+import com.khoders.tsm.entities.ProductPackage;
+import com.khoders.tsm.entities.UnitMeasurement;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
@@ -137,5 +140,35 @@ public class SalesService
         }
         return Collections.emptyList();
     }
-
+   public List<ProductPackage> queryPackagePrice(Product product)
+    {
+        try
+        {
+            return crudApi.getEm().createQuery("SELECT e FROM ProductPackage e WHERE e.product =?1", ProductPackage.class)
+                    .setParameter(1, product)
+                    .getResultList();
+            
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+    
+    
+    public double queryPackagePrice(UnitMeasurement unitMeasurement, Product product)
+    {
+        try
+        {
+            return crudApi.getEm().createQuery("SELECT e FROM ProductPackage e WHERE e.unitMeasurement =?1 AND e.product=?2", ProductPackage.class)
+                    .setParameter(1, unitMeasurement)
+                    .setParameter(2, product)
+                    .getSingleResult().getPackagePrice();
+            
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return 0.0;
+    }
 }
